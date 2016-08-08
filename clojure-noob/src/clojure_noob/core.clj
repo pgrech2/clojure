b7a45bf85dee6df51511b721f43166a0c61c9063
Paul Grech <pgrech@pgrech.laddersoffice.com>
2016-08-08 13:29:21 -0400

    first commit

---
+++ clojure-noob/src/clojure_noob/core.clj
@@ -0,0 +1,12 @@
+(ns clojure-noob.core
+  (:gen-class))
+
+(defn -main
+  "I don't do a whole lot ... yet."
+  [& args]
+  (println "my name is paul"))
+
+(defn -main2
+  "I don't do a whole lot ... yet."
+  [& args]
+  (println "my name is paul in main 2"))
